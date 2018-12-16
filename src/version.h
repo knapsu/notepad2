c3d3153e4fbf5c88c0d0acd85b7b0c3402ea8792
Krzysztof Knapik <knapsu@gmail.com>
2018-12-16 23:56:58 +0100

    Notepad2 4.2.25 PL

---
+++ src/version.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+*
+*
+* Notepad2
+*
+* version.h
+*   Notepad2 version information
+*
+* See Readme.txt for more information about this source code.
+* Please send me your comments to this work.
+*
+* See License.txt for details about distribution and modification.
+*
+*                                              (c) Florian Balmer 1996-2011
+*                                                  florian.balmer@gmail.com
+*                                               http://www.flos-freeware.ch
+*
+*
+******************************************************************************/
+
+
+#define VERSION_FILEVERSION_NUM      4,2,25,0
+#define VERSION_FILEVERSION_SHORT    L"4.2.25 PL"
+#define VERSION_FILEVERSION_LONG     L"Notepad2 4.2.25 PL"
+#define VERSION_LEGALCOPYRIGHT_SHORT L"Prawa autorskie © 2004-2011"
+#define VERSION_LEGALCOPYRIGHT_LONG  L"© Florian Balmer Krzysztof Knapik 2004-2011"
+#ifdef _M_AMD64
+#define VERSION_FILEDESCRIPTION      L"Notepad2 PL"
+#else
+#define VERSION_FILEDESCRIPTION      L"Notepad2 PL"
+#endif
+#define VERSION_INTERNALNAME         L"Notepad2"
+#define VERSION_ORIGINALFILENAME     L"Notepad2.exe"
+#define VERSION_AUTHORNAME           L"Florian Balmer"
+#define VERSION_AUTHORNAME_PL        L"Krzysztof Knapik"
+#define VERSION_WEBPAGEDISPLAY       L"http://www.flos-freeware.ch/"
+#define VERSION_WEBPAGEDISPLAY_PL    L"http://knapsu.eu/notepad2_pl/"
+#define VERSION_PAGEDESCRIPTION      L"Strona domowa programu:"
+#define VERSION_PAGEDESCRIPTION_PL   L"Strona domowa polskiej wersji programu:"
+#define VERSION_EMAILDISPLAY         L"florian.balmer@gmail.com"
+#define VERSION_EMAILDISPLAY_PL      L"knapsu@gmail.com"
